ff9b508c66abb92ddfe0246085b42104af3d3278
Robert Brignull <robertbrignull@gmail.com>
2020-07-01 12:26:51 +0100

    use review lab

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,10 +2,9 @@ import * as core from "@actions/core";
 import * as octokit from "@octokit/rest";
 import consoleLogLevel from "console-log-level";
 
-const githubAPIURL = process.env["GITHUB_API_URL"] || "https://api.github.com";
 export const client = new octokit.Octokit({
   auth: core.getInput("token"),
-  baseUrl: githubAPIURL,
+  baseUrl: 'https://codescanning-status-reports.review-lab.github.com/api/v3',
   userAgent: "CodeQL Action",
   log: consoleLogLevel({ level: "debug" })
 });
